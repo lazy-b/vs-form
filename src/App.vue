73ca174bfe52a5dee0e50fb737289785f06a5c7d
lazy-b <2280042554@qq.com>
2020-11-26 17:35:44 +0800

    refactor(config): 修改了校验规则 删除了一些无用页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Form />
   </div>
 </template>
 
+<script>
+import Form from '@/components/form/demo';
+
+export default {
+  name: 'App',
+  components: { Form },
+};
+</script>
+
 <style lang="scss">
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
 
   a {
-    font-weight: bold;
     color: #2c3e50;
+    font-weight: bold;
 
     &.router-link-exact-active {
       color: #42b983;
